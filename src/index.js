849a7f584703707b9db902f404f4173a43129a95
AlexandrSybirko <63520677+AlexandrSybirko@users.noreply.github.com>
2020-10-31 14:54:25 +0200

    создает разметку

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './styles.css';
+
+import countryCardTpl from './templates/country-card.hbs'
+
+const refs = {
+    cardContainer: document.querySelector('js-card-container')
+}
+
+
+fetch('https://restcountries.eu/rest/v2/name/ukraine').then(response => {
+   
+    return response.json();
+}).then(country => {
+    console.log(country);
+    const markup = countryCardTpl(country);
+    console.log(markup);
+    refs.cardContainer.innerHTML(markup)
+   
+}).catch(error => { console.log(error); });
+
+
+
