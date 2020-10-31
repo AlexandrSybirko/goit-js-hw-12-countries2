98d73f0d1518407c03aafc80372a90976427c840
AlexandrSybirko <63520677+AlexandrSybirko@users.noreply.github.com>
2020-10-31 19:54:23 +0200

    добавляет стили

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
+
+
 import './styles.css';
 
-import countryCardTpl from './templates/country-card.hbs'
-
-const refs = {
-    cardContainer: document.querySelector('js-card-container')
-}
-
-
-fetch('https://restcountries.eu/rest/v2/name/italy').then(response => {
-   
-    return response.json();
-}).then(country => {
-    console.log(country);
-    const markup = countryCardTpl(country[0]);
-    console.log(markup);
-    refs.cardContainer.innerHTML = markup
-}).catch(error => { console.log(error); });
-
-
-
+import './js/search.js'
--- src/js/search.js
+++ src/js/search.js
@@ -1,2 +1,69 @@
+import debounce from 'lodash.debounce';
+
+import API from './fetchCountries';
+import getRefs from './gets-refs'
+
+import countryCardTpl from '../templates/country-card.hbs'
+import countryListTpl from '../templates/country-list.hbs'
+
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+
+const refs = getRefs()
+let searchCountry = '';
+
+refs.searchInput.addEventListener(
+  'input',
+  debounce(() => {
+    onSearch();
+  }, 500),
+);
+
+
+function onSearch() {
+
+    searchCountry = refs.searchInput.value;
+    API.fetchCountries(searchCountry).then(countryCheck).catch(onFetchError);
+
+}
+
+function onFetchError(error) {
+  
+  console.log(error);
+}
+
+
+function countryCheck(country) {
+  if (country.length > 10) {
+    tooMany();
+    
+  } else if (country.length <= 10 && country.length > 1) {
+        rendercountryCard(countryListTpl, country);
+
+  } else if (country.length === 1) {
+    
+    rendercountryCard(countryCardTpl, country[0]);
+  } 
+}
+
+
+
+function rendercountryCard(template,country) {
+    const markup = template(country);
+    console.log(markup);
+    refs.cardContainer.innerHTML = markup
+}
+
+
+function tooMany() {
+  error({
+    title: 'Uh Oh!',
+    text: 'Too many matches found. Please enter a more specific query!',
+    
+  });
+}
+
 
 
